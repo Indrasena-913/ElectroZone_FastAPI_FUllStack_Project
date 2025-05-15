fa2261f035efbf00cfd0fedd308b639b2ef70545
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-16 00:12:47 +0530

    enhanced get orders

--- backend/routers/orders.py
+++ backend/routers/orders.py
@@ -27,6 +27,7 @@ async def create_order(user:user_dependency,db:db_dependency):
     db.add(order)
     db.commit()
     db.refresh(order)
+    print(cartItems)
 
     for item in cartItems:
         product = db.query(Product).filter(Product.id == item.product_id).first()
@@ -35,7 +36,7 @@ async def create_order(user:user_dependency,db:db_dependency):
         order_items=OrderItem(
                order_id=order.id,
                product_id=item.product.id,
-               price=item.product.price,
+               price=item.product.price*item.quantity,
                status=OrderStatus.PENDING 
           )
         db.add(order_items)
@@ -45,5 +46,38 @@ async def create_order(user:user_dependency,db:db_dependency):
     return {
         "message": "Order placed successfully",
         "order_id": order.id,
+        "order_items":order_items
     }
 
+@router.get("/orders",status_code=status.HTTP_200_OK)
+async def get_all_orders(user:user_dependency,db:db_dependency):
+    userId=user["id"]
+    if not userId:
+        raise HTTPException(status_code=401,detail="Unathorized")
+    orders=db.query(Order).filter(Order.user_id==userId).all()
+    if not orders:
+         raise HTTPException(status_code=404,detail="No orders found")
+    orders_list=[]
+    for order in orders:
+        orderItems=db.query(OrderItem).filter(OrderItem.order_id==order.id).all()
+        item_detail=[]
+        for item in orderItems:
+              product=db.query(Product).filter(Product.id==item.product_id).first()
+              if product:
+                   item_detail.append({
+                        "product_id": product.id,
+                    "title": product.title,
+                    "price": item.price,
+                    "status": item.status.value,
+                        
+                   })
+        orders_list.append({
+             "order_id":order.id,
+             "created_at":order.created_at,
+             "items":item_detail
+        })
+              
+    return {
+        "user_id": userId,
+        "allorders":orders_list
+    }
