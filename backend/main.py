a4e46d1da2361a675a8917a5f1f894ec4d8ce2dd
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-16 00:36:27 +0530

    added CORS

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,21 @@ from fastapi import FastAPI
 import models
 from database import Base,engine
 from routers import auth,products,cart,orders
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+
+
 app.include_router(auth.router)
 app.include_router(products.router)
 app.include_router(cart.router)
