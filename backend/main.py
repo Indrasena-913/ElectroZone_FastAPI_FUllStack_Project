fb71c27759201c213d9ecfe2c45d8424732d783b
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-16 14:55:27 +0530

    enhancing cors origins

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
+origins=["http://192.168.1.9:5500"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
