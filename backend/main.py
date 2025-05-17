95e7bcb914b5e983cdd3c9b43d163e6fe7efddb2
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-17 10:11:20 +0530

    aws changes

--- backend/main.py
+++ backend/main.py
@@ -3,21 +3,21 @@ import models
 from database import Base,engine
 from routers import auth,products,cart,orders
 from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 
 app=FastAPI()
 
-origins=["http://192.168.1.9:5500"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
 
-
+handler = Mangum(app)
 
 app.include_router(auth.router)
 app.include_router(products.router)
